766cb660a5129b17c96b791b429f18fe57b07f96
usr38259 <88048930+usr38259@users.noreply.github.com>
2023-06-15 08:51:25 +0300

    Add files via upload

--- cpuid80s.asm
+++ cpuid80s.asm
@@ -1,7 +1,7 @@
 
 ;CPUID for 8080/8085/z80 and compatibles, Version 1.6r
 ;Ivan Gorodetsky 20.01.2014, 08.01.2022
-;Adapted for "Specialist" retro PC / Syabr (by lexarr, 04.23)
+;Adapted for "Specialist" retro PC / Syabr (by lexarr, 06.23)
 ;
 ;Used some code and ideas from Ermolaev Sergey (SES), Perlin Vitaly (PPC),
 ; Slavinsky Vjacheslav (svofski)
@@ -11,18 +11,13 @@
 include macros80.inc
 
 putstr	EQU	0C438h
-putc	EQU	0C037h
 
-	call	nl
 	lxi 	h, msg_cpu
 	call 	putstr
 	call 	cpudetect
 	call	putstr
-	call	nl
 	ret
-nl:	mvi	c, 0Ah
-	call	putc
-	ret
+
 cpudetect:
 	sub	a
 	jpo	difz80
@@ -60,12 +55,12 @@ difz80:
 	lxi	h, msg_z80
 	ret
 
-msg_cpu		db	"CPU: ", 0
-msg_z80		db	"Z80", 0
-msg_r800	db	"R800", 0
-msg_i8080	db	"I8080", 0
-msg_amd8080	db	"AMD8080", 0
-msg_8085	db	"8085", 0
-msg_vm1		db	"580VM1", 0
+msg_cpu		db	0Ah, "CPU: ", 0
+msg_z80		db	"Z80", 0Ah, 0
+msg_r800	db	"R800", 0Ah, 0
+msg_i8080	db	"I8080", 0Ah, 0
+msg_amd8080	db	"AMD8080", 0Ah, 0
+msg_8085	db	"8085", 0Ah, 0
+msg_vm1		db	"580VM1", 0Ah, 0
 
 ENDALL
